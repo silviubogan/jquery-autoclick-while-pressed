1f391593860f6106910f66178bb5533c0d363ee0
Silviu Bogan <silviubogan@gmail.com>
2013-09-22 17:22:54 +0300

    Added source code

---
+++ autoclick-while-pressed.js
@@ -0,0 +1,32 @@
+(function ($) {
+	$.fn.autoclickWhilePressed = function (userOptions) {
+		var interval = null;
+		var initialDelayTimeout = null;
+		var options = {
+			intervalLength: 50,
+			initialDelay: 300,
+			eventToTrigger: "click"
+		};
+		if (typeof userOptions === "object") {
+			$.extend(options, userOptions);
+		}
+		var $this = this;
+		return this.on({
+			"mousedown": function (ev) {
+				initialDelayTimeout = setTimeout(function () {
+					interval = setInterval(function () {
+						$this.trigger(options.eventToTrigger);
+					}, options.intervalLength);
+				}, options.initialDelay);
+			},
+			"mouseup mouseleave": function () {
+				if (initialDelayTimeout !== null) {
+					clearTimeout(initialDelayTimeout);
+				}
+				if (interval !== null) {
+					clearInterval(interval);
+				}
+			}
+		});
+	};
+})(jQuery);
